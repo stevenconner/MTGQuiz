53c829fcb5c8665974637cdc188c315f3ff4766c
Steven Conner <steven.c.conner@gmail.com>
2016-10-10 13:25:09 -0700

    make button work correctly

--- app/src/main/java/com/example/android/mtgquiz/MainActivity.java
+++ app/src/main/java/com/example/android/mtgquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.mtgquiz;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -66,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             answeredCorrectly = answeredCorrectly + 1;
         }
     }
-    private void gradeAnswers() {
+    public void gradeAnswers(View view) {
         parseName();
         parseQuestion1();
         parseQuestion2();
@@ -74,10 +75,11 @@ public class MainActivity extends AppCompatActivity {
         parseQuestion4();
         parseQuestion5();
         Context context = getApplicationContext();
-        CharSequence text = name + ", You have answered " + answeredCorrectly + " Out of 5 questions correctly!";
+        CharSequence text = name + ", You have answered " + answeredCorrectly + " out of 5 questions correctly!";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+        answeredCorrectly = 0;
     }
 }
